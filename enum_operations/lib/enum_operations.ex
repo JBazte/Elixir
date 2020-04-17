aa3b897e0c17fd06d13dc6f0a11e5593e74d5ec9
Joaquín Bazterrica <joaquin.bazterrica@gmail.com>
2020-04-17 10:16:35 -0300

    Added doc_test :D

--- enum_operations/lib/enum_operations.ex
+++ enum_operations/lib/enum_operations.ex
@@ -16,13 +16,59 @@ defmodule EnumOperations do
     :world
   end
 
+  @doc """
+  Count
+
+  ## Examples
+
+      iex> EnumOperations.count([1,2,3,4,5])
+      5
+
+  """
+  
   def count(n) do
     count(n, 1)
   end
+  
+  defp count(n, c) do
+    if tl(n) != [] do
+      count(tl(n), c + 1)
+    else
+      c
+    end
+  end
+  
+  @doc """
+  Reverse
+
+  ## Examples
+
+      iex> EnumOperations.reverse([1,2,3,4,5])
+      [5, 4, 3, 2, 1]
+
+  """
 
   def reverse(n) do
     reverse(n, [])
   end
+
+  defp reverse(n, l) do
+    if tl(n) != [] do
+      reverse(tl(n), [hd(n) | l])
+    else
+      [hd(n) | l]
+    end
+  end
+
+  @doc """
+  Concat
+
+  ## Examples
+
+      iex> EnumOperations.concat([[1, 2, 3], [4,5]])
+      [1, 2, 3, 4, 5]
+
+  """
 
   def concat([]) do
     []
@@ -35,6 +81,16 @@ defmodule EnumOperations do
   def concat([[h | t] | tt]) do
     [h | concat([t | tt])]
   end
+
+  @doc """
+  Filter
+
+  ## Examples
+
+      iex> EnumOperations.filter([1, 2, 3, 4, 5], fn x -> rem(x, 2) == 0 end)
+      [2, 4]
+
+  """
 
   def filter([[] | t], r) do
     if t != [] do
@@ -55,30 +111,32 @@ defmodule EnumOperations do
       filter([[] | t], r)
     end
   end
-
-  defp count(n, c) do
-    aux = tl(n)
-
-    if aux != [] do
-      count(aux, c + 1)
-    else
-      c
-    end
-  end
-
-  defp reverse(n, l) do
-    if tl(n) != [] do
-      l = [hd(n) | l]
-      aux = tl(n)
-      reverse(aux, l)
-    else
-      l = [hd(n) | l]
-      l
-    end
-  end
 end
 
 defmodule FizzBuzz do
+  @moduledoc """
+    Documentation for `FizzBuzz`.
+  """
+
+  @doc """
+  Fuzzle
+
+  ## Examples
+
+      iex> FizzBuzz.fuzzle(0,0,1)
+      "FizzBuzz"
+
+      iex> FizzBuzz.fuzzle(0,1,1)
+      "Fizz"
+
+      iex> FizzBuzz.fuzzle(1,0,1)
+      "Buzz"
+
+      iex> FizzBuzz.fuzzle(1,1,0)
+      0
+
+  """
+
   def fuzzle(a, b, c) do
     if a == 0 and b == 0 and c != 0 do
       "FizzBuzz"
@@ -97,6 +155,31 @@ defmodule FizzBuzz do
 end
 
 defmodule WordCount do
+
+  @moduledoc """
+    Documentation for `WordCount`.
+  """
+
+  @doc """
+  WordCount
+
+  ## Examples
+
+      iex> WordCount.word_count("I like apples because apples go crunch in my mouth")
+      %{
+        "I" => 1,
+        "apples" => 2,
+        "because" => 1,
+        "crunch" => 1,
+        "go" => 1,
+        "in" => 1,
+        "like" => 1,
+        "mouth" => 1,
+        "my" => 1
+      }
+
+  """
+
   def word_count(p) do
     String.split(p)
     |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
